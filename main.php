942fdc087962b22669edf9844ae3e9ef51c52419
paolaola <paolaola9@gmail.com>
2018-09-21 03:33:45 -0500

    h

--- main.php
+++ main.php
@@ -1,9 +1,4 @@
 <?php
-$destino="paolaola9@gmail.com"
-$Name=$_POST["Name"];
-$Phone=$_POST["Phone"];
-$Email=$_POST["Email "];
-$contenido="Name: "$Name"- \nPhone: "$Phone" - \nEmail: "$Email;
-mail($destino, "contacto" , $contenido);
+
 header:("Location: /index.html") ;
 ?>
